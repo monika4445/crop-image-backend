6da650a8ac074226e1d205fdfef622e820f78ec0
Robert <robsahakyan54@gmail.com>
2022-08-30 21:38:28 +0400

    initial commit

--- cropimage-backend/App.js
+++ cropimage-backend/App.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app started listen at ${process.env.PORT || 3001} PORT`)
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`app started listen at ${process.env.PORT || 4000} PORT`)
 })
