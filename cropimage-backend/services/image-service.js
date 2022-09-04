000149c8fe11aaf82afe42d7bed191051cfdc9fc
monika4445 <86877899+monika4445@users.noreply.github.com>
2022-09-04 17:01:58 +0400

    Update image-service.js

--- cropimage-backend/services/image-service.js
+++ cropimage-backend/services/image-service.js
@@ -7,11 +7,15 @@ async function cropImage({x, y, width, height, offsetWidth, offsetHeight}) {
 
 // file name for cropped image
     let outputImage = 'uploads/croppedImage.jpg';
-    const metadata = await sharp(originalImage).metadata()
+    try {
+        const metadata = await sharp(originalImage).metadata();
+        } catch(error) {
+    console.log(`An error occurred during processing: ${error}`);
+  }
     const divisionOfWidth = metadata.width / offsetWidth;
     const divisionOfHeight = metadata.height / offsetHeight;
 
-    sharp(originalImage).extract({ width: Math.floor(width * divisionOfWidth), height: Math.floor(height * divisionOfHeight) , left: Math.floor(x * divisionOfWidth), top: Math.floor(y * divisionOfHeight)}).toFile(outputImage)
+   await sharp(originalImage).extract({ width: Math.floor(width * divisionOfWidth), height: Math.floor(height * divisionOfHeight) , left: Math.floor(x * divisionOfWidth), top: Math.floor(y * divisionOfHeight)}).toFile(outputImage)
         .then(function(new_file_info) {
             console.log("Image cropped and saved");
         })
