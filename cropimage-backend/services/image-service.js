bea74a993aff45f9a3e9cf926da52b4c6d96943a
Robert <robsahakyan54@gmail.com>
2022-09-05 15:46:02 +0400

    modified image-service

--- cropimage-backend/services/image-service.js
+++ cropimage-backend/services/image-service.js
@@ -5,19 +5,21 @@ async function cropImage({x, y, width, height, offsetWidth, offsetHeight}) {
     let outputImage = 'uploads/croppedImage.jpg';
     try {
         const metadata = await sharp(originalImage).metadata();
-        } catch(error) {
-    console.log(`An error occurred during processing: ${error}`);
-  }
-    const divisionOfWidth = metadata.width / offsetWidth;
-    const divisionOfHeight = metadata.height / offsetHeight;
+        const divisionOfWidth = metadata.width / offsetWidth;
+        const divisionOfHeight = metadata.height / offsetHeight;
 
-    sharp(originalImage).extract({ width: Math.round(width * divisionOfWidth), height: Math.round(height * divisionOfHeight) , left: Math.round(x * divisionOfWidth), top: Math.round(y * divisionOfHeight)}).toFile(outputImage)
+        sharp(originalImage).extract({ width: Math.round(width * divisionOfWidth), height: Math.round(height * divisionOfHeight) , left: Math.round(x * divisionOfWidth), top: Math.round(y * divisionOfHeight)}).toFile(outputImage)
         .then(function(new_file_info) {
             console.log("Image cropped and saved");
         })
         .catch(function(err) {
             console.log("An error occured",err);
     });
+        } catch(error) {
+    console.log(`An error occurred during processing: ${error}`);
+  }
+
+ 
 }
 
 export default {
